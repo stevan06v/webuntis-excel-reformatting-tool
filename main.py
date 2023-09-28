8f659396b7edde14172fa3f940fcc806386ce2f2
stevan06v <stevanvlajic5@gmail.com>
2023-09-28 22:34:37 +0200

    ..

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import filedialog
 import openpyxl
+import numpy as np
 
 # dictionary
 dictionary = {
@@ -14,15 +15,53 @@ dictionary = {
 # get all dictionary keys
 dictionary_key = list(dictionary.keys())
 
-file_name = ""
+available_months = list()
 
-while file_name == "":
+# TODO: Change later on to -> ""
+file_path = "test.xlsx"
+
+while file_path == "":
     print("No file selected.\nRestarting...")
     # start up dialog
-    file_name = filedialog.askopenfilename(
+    file_path = filedialog.askopenfilename(
         title="Open Text File",
-        filetypes=(("Excel Files", "*.xls"),))
+        filetypes=(("Excel Files", "*.xlsx"),))
 
-print(file_name)
+wb = openpyxl.load_workbook(file_path)
+
+print("You have successfully selected: " + file_path)
+
+ws = wb['Sheet1']
+
+# final values
+starting_date_cell_column = 2
+ending_date_cell_column = 11
+date_row = 2
+
+event_dictionary = dict()
+
+shape = (ending_date_cell_column, 33)
+dates_for_month = np.empty(shape, dtype='str')
+
+count_nulls = 0
 
 
+
+# add data to 2d array
+for i in range(2, ending_date_cell_column):
+    for j in range(3, 33):
+        current_value = ws.cell(row=j, column=i).value
+        count_nulls += 1
+        if current_value is not None:
+            dates_for_month[i - 2][j - 3 - count_nulls] = current_value
+    count_nulls = 0
+
+
+# get available months
+for i in range(2, ending_date_cell_column):
+    available_months.append(ws.cell(row=2, column=i).value)
+
+for month, dates in zip(available_months, dates_for_month):
+    event_dictionary[month] = dates
+
+print(event_dictionary)

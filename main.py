5bc6f1e68a4db0af717b5a54d559e9eb03c0af5d
stevan06v <stevanvlajic5@gmail.com>
2023-09-28 20:27:24 +0200

    feat: added loop to file-dialog

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from tkinter import filedialog
 
-# start up dialog
-file_name = filedialog.askopenfilename(
-    title="Open Text File",
-    filetypes=(("Excel Files", "*.xls"),))
+# dictionary
+dictionary = {
+    "EBA E1": "Englisch",
+    "0WRRE": "Recht",
+    "0WRWI": "Wirtschaft",
+    "0MEDTSM": "Mobile Computing",
+    "0D": "Deutsch",
+    "1SEW": "Softwareentwicklung",
+    "1INSY": "Datenbanken",
+}
+# get all dictionary keys
+dictionary_key = list(dictionary.keys())
 
+file_name = ""
+
+while file_name == "":
+    print("No file selected.\nRestarting...")
+    # start up dialog
+    file_name = filedialog.askopenfilename(
+        title="Open Text File",
+        filetypes=(("Excel Files", "*.xls"),))
 
 print(file_name)
+
+

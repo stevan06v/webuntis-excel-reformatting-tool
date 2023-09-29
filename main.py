408e6aca8d32d160516c8b3bbae16aa372076169
Stevan <stevanvlajic5@gmail.com>
2023-09-29 15:16:35 +0200

    feat: model deisgned

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 from tkinter import filedialog
 import openpyxl
 import numpy as np
+import re
+import datetime
+
+
+class Assignment:
+
+    def __init__(self, _subject, _day, _month, _type):
+        self.subject = _subject
+        self.type = _type
+        self.date = self.create_date_of_month_day(_day, _month)
+
+    def create_date_of_month_day(self, _day, _month):
+        formatted_day = int(self.add_zero_to_single_digit_numbers(_day))
+        current_year = int(datetime.datetime.now().year)
+
+        return datetime.datetime(year=current_year, month=_month, day=formatted_day)
+
+    def add_zero_to_single_digit_numbers(self, input_string):
+        if len(input_string) == 1:
+            return "0" + input_string
+        else:
+            return input_string
 
 # dictionary
 dictionary = {
@@ -12,6 +34,7 @@ dictionary = {
     "1SEW": "Softwareentwicklung",
     "1INSY": "Datenbanken",
 }
+
 # get all dictionary keys
 dictionary_key = list(dictionary.keys())
 
@@ -38,31 +61,17 @@ starting_date_cell_column = 2
 ending_date_cell_column = 11
 date_row = 2
 
-event_dictionary = dict()
-
-shape = (ending_date_cell_column, 33)
-dates_for_month = np.empty(shape, dtype='str')
-
-count_nulls = 0
-
-
-
+# all list
+assignment_list = set()
 
 # add data to 2d array
 for i in range(2, ending_date_cell_column):
     for j in range(3, 33):
-        current_value = ws.cell(row=j, column=i).value
-        count_nulls += 1
-        if current_value is not None:
-            dates_for_month[i - 2][j - 3 - count_nulls] = current_value
-    count_nulls = 0
+        if ws.cell(row=j, column=i).value is not None:
+            _day = ""
+            _month = ws.cell(row=j, column=i).value
+            _subject = ""
+            _type = ""
+            assignment_list.add(Assignment(_subject, _day, _month, _type))
 
 
-# get available months
-for i in range(2, ending_date_cell_column):
-    available_months.append(ws.cell(row=2, column=i).value)
-
-for month, dates in zip(available_months, dates_for_month):
-    event_dictionary[month] = dates
-
-print(event_dictionary)
